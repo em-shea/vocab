6da9b186d52789fff010404f9824b22a1e579f15
em-shea <c.emilyshea@gmail.com>
2018-12-12 22:35:41 -0800

    edit text message body

--- TextFunction/TextFunction.py
+++ TextFunction/TextFunction.py
@@ -3,24 +3,27 @@ import boto3
 import os
 from botocore.vendored import requests
 
+print("hello")
+
 lambda_client = boto3.client('lambda')
 sns_client = boto3.client('sns')
+
+print("hello")
 
 def lambda_handler(event, context):
     """Calls VocabRandomEntry function"""
     invoke_response = lambda_client.invoke(
-        FunctionName="VocabEntrySelector",
+        FunctionName="VocabRandomTest",
         InvocationType='RequestResponse'
-        })
     )
     response_json = invoke_response['Payload'].read()
     response_python = json.loads(response_json)
     word = response_python["body"]
 
-message = {"foo": "bar"}
+    message = word["Word"] + "\n" + word["Pronunciation"] + "\n" + word["Definition"]
 
-response = client.publish(
-    TargetArn=arn:aws:sns:us-east-1:789896561553:Vocab,
-    Message=json.dumps({'default': json.dumps(message)}),
-    MessageStructure='json'
-)
+    response = sns_client.publish(
+        TargetArn='arn:aws:sns:us-east-1:789896561553:Vocab',
+        Message=json.dumps({'default': message}),
+        MessageStructure='json'
+    )
