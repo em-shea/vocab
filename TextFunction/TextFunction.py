0177cddbe6d37bb4c1796a183f762b1d942c5a7e
em-shea <c.emilyshea@gmail.com>
2018-12-12 22:50:46 -0800

    Added publish URL to sns

--- TextFunction/TextFunction.py
+++ TextFunction/TextFunction.py
@@ -13,17 +13,24 @@ print("hello")
 def lambda_handler(event, context):
     """Calls VocabRandomEntry function"""
     invoke_response = lambda_client.invoke(
-        FunctionName="VocabRandomTest",
+        FunctionName="VocabRandomEntry",
         InvocationType='RequestResponse'
     )
     response_json = invoke_response['Payload'].read()
     response_python = json.loads(response_json)
     word = response_python["body"]
 
-    message = word["Word"] + "\n" + word["Pronunciation"] + "\n" + word["Definition"]
+    message = "\n" + word["Word"] + "\n" + word["Pronunciation"] + "\n" + word["Definition"]
+    baidu_link = "https://fanyi.baidu.com/#zh/en/" + word["Word"]
 
     response = sns_client.publish(
         TargetArn='arn:aws:sns:us-east-1:789896561553:Vocab',
         Message=json.dumps({'default': message}),
+        MessageStructure='json'
+    )
+    
+    response = sns_client.publish(
+        TargetArn='arn:aws:sns:us-east-1:789896561553:Vocab',
+        Message=json.dumps({'default': baidu_link}),
         MessageStructure='json'
     )
