7fe011083284cc98ab936b8ffdbedd908ce53fb4
em-shea <c.emilyshea@gmail.com>
2018-12-16 12:42:07 -0800

    Working on levels in textfunction

--- TextFunction/TextFunction.py
+++ TextFunction/TextFunction.py
@@ -3,12 +3,18 @@ import boto3
 import os
 from botocore.vendored import requests
 
-print("hello")
-
 lambda_client = boto3.client('lambda')
 sns_client = boto3.client('sns')
 
-print("hello")
+def get_random(any_level):
+    invoke_response = lambda_client.invoke(
+        FunctionName="VocabRandomEntry",
+        InvocationType='RequestResponse')
+
+        response_json = invoke_response['Payload'].read()
+        response_python = json.loads(response_json)
+        word = response_python["body"]
+        return: word
 
 def lambda_handler(event, context):
     """Sends a daily random HSK vocab to SNS subscribers"""
@@ -40,21 +46,14 @@ def lambda_handler(event, context):
     }
    ]
     
-    for level in hsk_level_arns,
-    
-        invoke_response = lambda_client.invoke(
-            FunctionName="VocabRandomEntry",
-            InvocationType='RequestResponse'
-        )
-        response_json = invoke_response['Payload'].read()
-        response_python = json.loads(response_json)
-        word = response_python["body"]
+    for level in hsk_level_arns:
+        get_random(level)
 
         # Selects relevant parts of the word's dictionary and creates baidu URL
         message = "\n" + word["Word"] + "\n" + word["Pronunciation"] + "\n" + word["Definition"]
         baidu_link = "https://fanyi.baidu.com/#zh/en/" + word["Word"]
 
-        # Publishes word, pronunciatino and definition to SNS
+        # Publishes word, pronunciation and definition to SNS
         response = sns_client.publish(
             TargetArn=os.environ['SNS_TOPIC_ARN'],
             Message=json.dumps({'default': message}),
