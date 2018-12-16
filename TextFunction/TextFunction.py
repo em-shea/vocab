d7640bd7c1b20f98d713003c6135509b3dfec5e1
em-shea <c.emilyshea@gmail.com>
2018-12-16 13:26:25 -0800

    Work on passing hsk level and filtering by it

--- TextFunction/TextFunction.py
+++ TextFunction/TextFunction.py
@@ -9,7 +9,10 @@ sns_client = boto3.client('sns')
 def get_random(any_level):
     invoke_response = lambda_client.invoke(
         FunctionName="VocabRandomEntry",
-        InvocationType='RequestResponse')
+        InvocationType='RequestResponse',       
+        Payload=json.dumps({
+            "hsk_level": any_level
+        })
 
         response_json = invoke_response['Payload'].read()
         response_python = json.loads(response_json)
@@ -19,35 +22,29 @@ def get_random(any_level):
 def lambda_handler(event, context):
     """Sends a daily random HSK vocab to SNS subscribers"""
     # Calls VocabRandomEntry, receives a single word's dictionary back
-    hsk_level_arns = [
-    {
-      hsk_level: "1",
-      topic_arn: os.environ['SNS_TOPIC_ARN1'],
-    },
-    {
-      hsk_level: "2",
-      topic_arn: os.environ['SNS_TOPIC_ARN2'],
-    },
-    {
-      hsk_level: "3",
-      topic_arn: os.environ['SNS_TOPIC_ARN3'],
-    }
-    {
-      hsk_level: "4",
-      topic_arn: os.environ['SNS_TOPIC_ARN4'],
-    }
-    {
-      hsk_level: "5",
-      topic_arn: os.environ['SNS_TOPIC_ARN5'],
-    }
-    {
-      hsk_level: "6",
-      topic_arn: os.environ['SNS_TOPIC_ARN6'],
-    }
-   ]
+    hsk_level_arns = [{
+        hsk_level: "1",
+        topic_arn: os.environ['SNS_TOPIC_ARN1'],
+    },{
+        hsk_level: "2",
+        topic_arn: os.environ['SNS_TOPIC_ARN2'],
+    },{
+        hsk_level: "3",
+        topic_arn: os.environ['SNS_TOPIC_ARN3'],
+    },{
+        hsk_level: "4",
+        topic_arn: os.environ['SNS_TOPIC_ARN4'],
+    },{
+        hsk_level: "5",
+        topic_arn: os.environ['SNS_TOPIC_ARN5'],
+    },{
+        hsk_level: "6",
+        topic_arn: os.environ['SNS_TOPIC_ARN6'],
+    }]
     
-    for level in hsk_level_arns:
-        get_random(level)
+    for level_dict in hsk_level_arns:
+        level = level_dict["hsk_level"]
+        word = get_random(level)
 
         # Selects relevant parts of the word's dictionary and creates baidu URL
         message = "\n" + word["Word"] + "\n" + word["Pronunciation"] + "\n" + word["Definition"]
--- VocabRandomEntry/VocabRandomEntry.py
+++ VocabRandomEntry/VocabRandomEntry.py
@@ -18,16 +18,28 @@ def random_entry(any_list):
 def lambda_handler(event, context):
     """Pulls a random vocabulary word from S3"""
     # Pulls data file from S3 bucket provided as an env var"""
+    print(event)
+    print(event["hsk_level"])
+    hsk_level = event["hsk_level"]
+
     csv_file = s3.get_object(Bucket=os.environ['S3_BUCKET_NAME'], Key=os.environ['S3_BUCKET_KEY'])
     csv_response = csv_file['Body'].read()
     stream = io.StringIO(csv_response.decode("utf-8"))
     
     # Reads and prints the CSV file as a list of dictionaries
+    filter_level = {hsk_level: 1}
+    with open('data.tsv', 'r') as f_name:
+        for line in csv.DictReader(stream, delimiter='\t'):
+            if line['HSK Level'] not in filter_countries:
+                print(line)
+    
+
     reader = csv.DictReader(stream)
     vocab_list = []
     for row in reader:
         vocab_list.append(dict(row))
     print(vocab_list)
+
 
     # Calls the random_entry function on the list
     word = random_entry(vocab_list)
