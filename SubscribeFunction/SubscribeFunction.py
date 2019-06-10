196d3c8562e5f8efda169e9fcefe3d148af452ad
em-shea <c.emilyshea@gmail.com>
2019-06-09 19:46:17 -0700

    Change out sub function for SG

--- SubscribeFunction/SubscribeFunction.py
+++ SubscribeFunction/SubscribeFunction.py
@@ -1,26 +1,85 @@
-import boto3
+import os
+from botocore.vendored import requests
+
+def add_to_contact_list(recipient_id, hsk_level):
+
+    list_ids = [
+        {
+            "id": 7697668,
+            "name": "Level 1"
+        },
+        {
+            "id": 8295693,
+            "name": "Level 2"
+        },
+        {
+            "id": 8296149,
+            "name": "Level 3"
+        },
+        {
+            "id": 8296153,
+            "name": "Level 4"
+        },
+        {
+            "id": 8393613,
+            "name": "Level 5"
+        },
+        {
+            "id": 8393614,
+            "name": "Level 6"
+        }
+    ]
+
+    hsk_level_index = hsk_level - 1
+
+    list_id = list_ids[hsk_level_index]["id"]
+
+    url = "https://api.sendgrid.com/v3/contactdb/lists/" + str(list_id) + "/recipients/" + str(recipient_id)
+
+    payload = "null"
+    headers = {'authorization': 'Bearer ' + os.environ['SG_API_KEY']}
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+    
+    print(response)
 
 def lambda_handler(event, context):
   
-  # hsk_level = event["queryStringParameters"]['hsk_level']
-  email_address = event["queryStringParameters"]['email']
+    # extract relevant user details
+    email_address = event["queryStringParameters"]['email']
+    hsk_level = event["queryStringParameters"]['level']
 
-  # Add dictionary with SNS topic ARNs by level
+    # create payload with user details
+    payload = [
+        {
+            "email" : email_address,
+            "level_list" : "Level" + hsk_level
+        }
+    ]
 
-  MY_SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:789896561553:vocab-Level1Topic-13PMNB2U5WC84'
-  sns_client = boto3.client('sns')
-  sns_client.subscribe(
-      TopicArn = MY_SNS_TOPIC_ARN,
-      Protocol = 'email',
-      Endpoint = email_address
-  )
+    # SendGrid create contact API endpoint and API key
+    url = "https://api.sendgrid.com/v3/contactdb/recipients"
 
-  return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Methods': 'POST,OPTIONS',
-            # 'Access-Control-Allow-Origin': os.environ['DomainName'],
-            'Access-Control-Allow-Origin': '*',
-        },
-        'body': '{"success" : true}'
+    headers = {
+        'authorization' : "Bearer " + os.environ['SG_API_KEY'],
+        'content-type' : "application/json"
     }
+
+    # Send POST request
+    response = requests.request("POST", url, json=payload, headers=headers)
+
+    # Collect ID from newly created contact
+    data = response.json()
+    recipient_id = data["persisted_recipients"][0]
+
+    # Call function to add new contact to list
+    add_to_contact_list(recipient_id, int(hsk_level))
+
+    return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Methods': 'POST,OPTIONS',
+                'Access-Control-Allow-Origin': '*',
+            },
+            'body': '{"success" : true}'
+        }
