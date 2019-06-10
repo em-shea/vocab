bdebb98ad97afb94ca625a32c40b2031edfc522e
em-shea <c.emilyshea@gmail.com>
2019-06-09 19:52:04 -0700

    Comment out SNS

--- TextFunction/TextFunction.py
+++ TextFunction/TextFunction.py
@@ -21,51 +21,51 @@ def get_random(any_level):
     word = response_python["body"]
     return word
 
-def lambda_handler(event, context):
-    """Sends a daily random HSK vocab to SNS subscribers"""
-    # Calls VocabRandomEntry, receives a single word's dictionary back
-    hsk_level_arns = [{
-        "hsk_level": "1",
-        "topic_arn": os.environ['SNS_TOPIC_ARN1'],
-    },{
-        "hsk_level": "2",
-        "topic_arn": os.environ['SNS_TOPIC_ARN2'],
-    },{
-        "hsk_level": "3",
-        "topic_arn": os.environ['SNS_TOPIC_ARN3'],
-    },{
-        "hsk_level": "4",
-        "topic_arn": os.environ['SNS_TOPIC_ARN4'],
-    },{
-        "hsk_level": "5",
-        "topic_arn": os.environ['SNS_TOPIC_ARN5'],
-    },{
-        "hsk_level": "6",
-        "topic_arn": os.environ['SNS_TOPIC_ARN6'],
-    }]
+# def lambda_handler(event, context):
+#     """Sends a daily random HSK vocab to SNS subscribers"""
+#     # Calls VocabRandomEntry, receives a single word's dictionary back
+#     hsk_level_arns = [{
+#         "hsk_level": "1",
+#         "topic_arn": os.environ['SNS_TOPIC_ARN1'],
+#     },{
+#         "hsk_level": "2",
+#         "topic_arn": os.environ['SNS_TOPIC_ARN2'],
+#     },{
+#         "hsk_level": "3",
+#         "topic_arn": os.environ['SNS_TOPIC_ARN3'],
+#     },{
+#         "hsk_level": "4",
+#         "topic_arn": os.environ['SNS_TOPIC_ARN4'],
+#     },{
+#         "hsk_level": "5",
+#         "topic_arn": os.environ['SNS_TOPIC_ARN5'],
+#     },{
+#         "hsk_level": "6",
+#         "topic_arn": os.environ['SNS_TOPIC_ARN6'],
+#     }]
     
-    for level_dict in hsk_level_arns:
-        level = level_dict["hsk_level"]
-        word = get_random(level)
-        num_level = int(level)
+#     for level_dict in hsk_level_arns:
+#         level = level_dict["hsk_level"]
+#         word = get_random(level)
+#         num_level = int(level)
 
-        message = ("HSK Level: " + level_dict["hsk_level"] + "\n" + word["Word"] + 
-            "\n" + word["Pronunciation"] + "\n" + word["Definition"]) 
+#         message = ("HSK Level: " + level_dict["hsk_level"] + "\n" + word["Word"] + 
+#             "\n" + word["Pronunciation"] + "\n" + word["Definition"]) 
         
-        if num_level in range(1,4):
-            url = "\n \n" + "https://www.yellowbridge.com/chinese/sentsearch.php?word=" + word["Word"]
+#         if num_level in range(1,4):
+#             url = "\n \n" + "https://www.yellowbridge.com/chinese/sentsearch.php?word=" + word["Word"]
   
-        else: 
-            url = "\n \n" + "https://fanyi.baidu.com/#zh/en/" + word["Word"]
+#         else: 
+#             url = "\n \n" + "https://fanyi.baidu.com/#zh/en/" + word["Word"]
 
-        complete_message = message + url
+#         complete_message = message + url
 
-        # Publishes word, pronunciation and definition to SNS
-        response = sns_client.publish(
-            TargetArn=level_dict["topic_arn"],
-            Message=json.dumps({'default': complete_message}),
-            MessageStructure='json'
-        )
+#         # Publishes word, pronunciation and definition to SNS
+#         response = sns_client.publish(
+#             TargetArn=level_dict["topic_arn"],
+#             Message=json.dumps({'default': complete_message}),
+#             MessageStructure='json'
+#         )
     
 
         
