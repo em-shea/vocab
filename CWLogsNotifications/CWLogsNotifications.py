ae597c5c5710b17c26c0f7e70a8e745b47c15535
em-shea <c.emilyshea@gmail.com>
2019-08-04 17:56:10 -0700

    CWLogs function decoding

--- CWLogsNotifications/CWLogsNotifications.py
+++ CWLogsNotifications/CWLogsNotifications.py
@@ -1,11 +1,29 @@
 import os
+import io
+import json
+import gzip
 import boto3
+import base64
 
 sns_client = boto3.client('sns')
 
 def lambda_handler(event, context):
 
-  message = "Notification Message"
+  print(event)
+  
+  #capture the CloudWatch log data
+  outEvent = event['awslogs']['data']
+  outEventDecoded = base64.b64decode(outEvent)
+  
+  #decode and unzip the log data
+  outEvent = gzip.decompress(outEventDecoded).decode('utf-8')
+  
+  # #convert the log data from JSON into a dictionary
+  # cleanEvent = json.loads(outEvent)
+
+  # print(cleanEvent)
+  
+  message = outEvent
 
   response = sns_client.publish(
           TargetArn = os.environ['SUB_TOPIC_ARN'], 
