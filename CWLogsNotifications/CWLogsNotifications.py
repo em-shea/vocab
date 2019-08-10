e3166b0c26aa452e65a88fd485cee2fca5767ba6
em-shea <c.emilyshea@gmail.com>
2019-08-10 16:27:12 -0700

    Add compose message to logging function

--- CWLogsNotifications/CWLogsNotifications.py
+++ CWLogsNotifications/CWLogsNotifications.py
@@ -4,21 +4,15 @@ import json
 import gzip
 import boto3
 import base64
+from datetime import datetime
 
 sns_client = boto3.client('sns')
 
 def lambda_handler(event, context):
   
-  #capture the CloudWatch log data
-  outEvent = event['awslogs']['data']
-  outEventDecoded = base64.b64decode(outEvent)
-  
-  #decode and unzip the log data
-  outEvent = gzip.decompress(outEventDecoded).decode('utf-8')
-  
-  message = outEvent
+  log_json = decode_and_decompress_log(event)
 
-  print(outEvent)
+  message = compose_message(log_json)
 
   response = sns_client.publish(
           TargetArn = os.environ['SUB_TOPIC_ARN'], 
@@ -27,3 +21,46 @@ def lambda_handler(event, context):
       )
 
   print("SNS Response", response)
+
+def decode_and_decompress_log(event):
+
+  # Capture the CloudWatch log data
+  outEvent = event['awslogs']['data']
+  outEventDecoded = base64.b64decode(outEvent)
+  
+  # Decode and unzip the log data
+  outEvent = gzip.decompress(outEventDecoded).decode('utf-8')
+  
+  log_json = json.loads(outEvent)
+
+  return log_json
+
+def compose_message(log_json):
+
+  message_dict = {}
+
+  # Function name
+  message_dict["function name"] = log_json["logGroup"].replace("/aws/lambda/","")
+
+  # Link
+  date_timestamp = datetime.fromtimestamp(log_json['logEvents'][0]["timestamp"]/1000).strftime('%Y-%m-%dT%H:%M:%SZ')
+  message_dict["link"] = f"https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#logEventViewer:group={log_json['logGroup']};stream={log_json['logStream']};start={date_timestamp}"
+
+  # Errors
+  message_dict["errors list"] = []
+  for error in log_json["logEvents"]:
+    
+    error_dict = {}
+
+    date_timestamp = datetime.fromtimestamp(error["timestamp"]/1000)
+    error_dict["time"] = date_timestamp.strftime('%Y-%m-%d %H:%M:%S')
+
+    error_dict["error message"] = error["message"]
+
+    message_dict["errors list"].append(error_dict)
+
+  print(message_dict)
+
+  return message_dict
+    
+compose_message(log_json)
