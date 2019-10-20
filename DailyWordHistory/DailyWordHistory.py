43b152b3386871fec42f898ddadd6c0231be847f
em-shea <c.emilyshea@gmail.com>
2019-10-20 12:55:30 -0500

    condense daily word history

--- DailyWordHistory/DailyWordHistory.py
+++ DailyWordHistory/DailyWordHistory.py
@@ -7,100 +7,92 @@ from boto3.dynamodb.conditions import Key
 from botocore.exceptions import ClientError
 
 dynamo = boto3.resource('dynamodb')
+table = dynamo.Table(os.environ['TABLE_NAME'])
+
+all_lists = [
+    'HSKLevel1',
+    'HSKLevel2',
+    'HSKLevel3',
+    'HSKLevel4',
+    'HSKLevel5',
+    'HSKLevel6'
+  ]
 
 def lambda_handler(event,context):
 
-  table = dynamo.Table(os.environ['TABLE_NAME'])
-
+  # Words returned from Dynamo
   items = []
 
-  from_date = datetime.today() - timedelta(days=int(90))
-
-  from_date = format_date(from_date)
+  # Set today's date and dates 30 and 90 days before today's date
   todays_date = format_date(datetime.today())
 
-  # Filter data by query string parameters
+  # Extract query string parameters
   # Query string parameters should be a the list name (ex, HSKLevel1) and a number of days (ex, 7)
   if 'queryStringParameters' in event:
 
-    # Filter by word list
+    # Set word list
     if 'list' in event['queryStringParameters']:
       list_id = event["queryStringParameters"]['list']
 
-      # Filter by number of days to include
+      # Set date range
       if 'date_range' in event["queryStringParameters"]:
         date_range = event["queryStringParameters"]['date_range']
 
-        # Subtract days to get date range
-        from_date = datetime.today() - timedelta(days=int(date_range))
+        from_date = format_date(datetime.today() - timedelta(days=int(date_range)))
 
-        from_date = format_date(from_date)
-        todays_date = format_date(datetime.today())
-
-        try:
-            response = table.query(
-                KeyConditionExpression=Key('ListId').eq(list_id) & Key('Date').between(str(from_date),str(todays_date))
-            )
-        except ClientError as e:
-            print(e.response['Error']['Message'])
-        else:
-            items = response['Items']
-            print(json.dumps(items, indent=4))
-
-            return {
-                'statusCode': 200,
-                'headers': {
-                    'Access-Control-Allow-Methods': 'GET,OPTIONS',
-                    # 'Access-Control-Allow-Origin': os.environ['DomainName'],
-                    'Access-Control-Allow-Origin': '*',
-                },
-                'body': json.dumps(items)
-            }
-
-      # If no date range supplied, get words from last 90 days
+      # If no date param given, set to 90 days
       else:
-          try:
-              response = table.query(
-                  KeyConditionExpression=Key('ListId').eq(list_id) & Key('Date').between(str(from_date),str(todays_date))
-              )
-          except ClientError as e:
-              print(e.response['Error']['Message'])
-          else:
-              items = response['Items']
-              print(json.dumps(items, indent=4))
+        from_date = format_date(datetime.today() - timedelta(days=int(90)))
 
-              return {
-                'statusCode': 200,
-                'headers': {
-                    'Access-Control-Allow-Methods': 'GET,OPTIONS',
-                    # 'Access-Control-Allow-Origin': os.environ['DomainName'],
-                    'Access-Control-Allow-Origin': '*',
-                },
-                'body': json.dumps(items)
-            }
+    items = pull_words_with_params(list_id, from_date)
 
-  # If no list or date range supplied, get all lists words from the last 30 days
+  # If no params passed, get all lists words from the last 30 days
   else:
-      try:
-          response = table.query(
-            KeyConditionExpression=Key('ListId').between('HSKLevel1','HSKLevel6') & Key('Date').between(str(from_date),str(todays_date))
-          )
-      except ClientError as e:
-          print(e.response['Error']['Message'])
-      else:
-          items = response['Items']
-          print(json.dumps(items, indent=4))
-          return items
+    items = pull_words_no_params()
 
   return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Methods': 'GET,OPTIONS',
-            # 'Access-Control-Allow-Origin': os.environ['DomainName'],
-            'Access-Control-Allow-Origin': '*',
-        },
-        'body': json.dumps(items)
-    }
+          'statusCode': 200,
+          'headers': {
+              'Access-Control-Allow-Methods': 'GET,OPTIONS',
+              # 'Access-Control-Allow-Origin': os.environ['DomainName'],
+              'Access-Control-Allow-Origin': '*',
+          },
+          'body': json.dumps(items)
+        }
+
+def pull_words_with_params(list_id, from_date):
+
+  try:
+      response = table.query(
+        KeyConditionExpression=Key('ListId').eq(list_id) & Key('Date').between(str(from_date),str(todays_date))
+      )
+  except ClientError as e:
+    print(e.response['Error']['Message'])
+  else:
+    items = response['Items']
+    print(json.dumps(items, indent=4))
+
+  return items
+
+def pull_words_no_params():
+
+  completed_item_list = []
+
+  from_date = format_date(datetime.today() - timedelta(days=int(30)))
+
+  try:
+      for list_id in all_lists:
+
+        response = table.query(
+          KeyConditionExpression=Key('ListId').eq(list_id) & Key('Date').between(str(from_date),str(todays_date))
+        )
+  except ClientError as e:
+    print(e.response['Error']['Message'])
+  else:
+    completed_item_list.append(response['Items'])
+
+  print(json.dumps(completed_item_list, indent=4))
+  return completed_item_list
 
 def format_date(date_object):
 
