c625b5177d071c3b09f6002f5470da89a69c94cd
em-shea <c.emilyshea@gmail.com>
2020-02-12 20:41:23 -0800

    add selected_word to daily campaign send

--- SendDailyCampaign/SendDailyCampaign.py
+++ SendDailyCampaign/SendDailyCampaign.py
@@ -54,7 +54,7 @@ def lambda_handler(event, context):
             word_index = int(contact['ListId']) - 1
             # print("Word index:", word_index)
 
-            hsk_level = contact['ListId']
+            hsk_level = list_id[0]
             char_set = contact['CharacterSet']
 
             word = word_list[word_index]
@@ -64,12 +64,12 @@ def lambda_handler(event, context):
             # word will be None. If so, do not send an email.
             if word is not None:
 
-                campaign_contents = assemble_html_content(level, email, word, char_set)
+                campaign_contents = assemble_html_content(hsk_level, email, word, char_set)
 
                 try:
                     response = send_email(campaign_contents, email)
                 except Exception as e:
-                    print(f"Error: Failed to send email - {email}, {level}.")
+                    print(f"Error: Failed to send email - {email}, {hsk_level}.")
                     print(e)
             # else:
             #     print("Unsubscribed contact:", contact['SubscriberEmail'])
@@ -123,36 +123,36 @@ def scan_contacts_table():
     return all_contacts
 
 # Populate relevant content in for the placeholders in the email template
-def assemble_html_content(level, email, word, char_set):
+def assemble_html_content(hsk_level, email, word, char_set):
 
     # Select simplified or traditional character 
     if char_set == "simplified":
-        word = word["Word"]
+        selected_word = word["Word"]
     else:
-        word = word["Word-Traditional"]
+        selected_word = word["Word-Traditional"]
 
     # print("Assembling HTML content...")
-    num_level = int(level)
+    num_level = int(hsk_level)
 
     # Create example sentence URL
     if num_level in range(1,4):
-        example_link = "https://www.yellowbridge.com/chinese/sentsearch.php?word=" + word
+        example_link = "https://www.yellowbridge.com/chinese/sentsearch.php?word=" + selected_word
 
     else:
-        example_link = "https://fanyi.baidu.com/#zh/en/" + word
+        example_link = "https://fanyi.baidu.com/#zh/en/" + selected_word
 
     # We have an html template file packaged with this function's code which we read here
     with open('template.html') as fh:
         contents = fh.read()
 
     # Replace relevant content in example template
-    campaign_contents = contents.replace("{word}", word)
+    campaign_contents = contents.replace("{word}", selected_word)
     campaign_contents = campaign_contents.replace("{pronunciation}", word["Pronunciation"])
     campaign_contents = campaign_contents.replace("{definition}", word["Definition"])
     campaign_contents = campaign_contents.replace("{link}", example_link)
-    campaign_contents = campaign_contents.replace("{level}", "HSK Level " + level)
-    campaign_contents = campaign_contents.replace("{history_link}", "https://haohaotiantian.com/history?list=HSKLevel" + level + "&dates=30")
-    campaign_contents = campaign_contents.replace("{unsubscribe_link}", "https://haohaotiantian.com/unsub?level=" + level + "&email=" + email)
+    campaign_contents = campaign_contents.replace("{level}", "HSK Level " + hsk_level)
+    campaign_contents = campaign_contents.replace("{history_link}", "https://haohaotiantian.com/history?list=HSKLevel" + hsk_level + "&dates=30")
+    campaign_contents = campaign_contents.replace("{unsubscribe_link}", "https://haohaotiantian.com/unsub?level=" + hsk_level + "&email=" + email)
 
     return campaign_contents
 
