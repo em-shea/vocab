0a289ce13eda518669b3db94f4238fd42e8e5ad5
Emily Shea <c.emilyshea@gmail.com>
2021-04-06 21:17:26 +0100

    create auth requirements

--- src/user_pool_triggers/define_auth_challenge/app.py
+++ src/user_pool_triggers/define_auth_challenge/app.py
@@ -6,38 +6,54 @@ def lambda_handler(event, context):
     request = event.get('request')
     session = request.get('session')
 
-
-    current_session = len(session) - 1
-
-    # If user not found, fail auth
-    if request.get('userNotFound') is True:
-        response.update({
-            'issueTokens': False,
-            'failAuthentication': True,+
-            'msg': "User does not exist"
-        }) elif session[current_session].get('challengeName') != 'CUSTOM_CHALLENGE':
-        # If user does not go through custom auth challenge flow, fail auth
-        response.update({
-            'issueTokens': False,
-            'failAuthentication': True,
-            'msg': 'User must use custom challenge to sign in'
-        }) elif len(session) >= 3 and session[2].get('challengeResult') is False:
-        # If user attempts 3 times with the wrong OTP, fail auth
-        response.update({
-            'issueTokens': False,
-            'failAuthentication': True,
-            'msg': 'Incorrect OTP after 3 attempts'
-        }) elif len(session) >0 and session[current_session].get('challengeName') == 'CUSTOM_CHALLENGE' and session[current_session].get('challengeResult') is True:
-        # Correct custom auth flow and OTP, succeed auth
-        response.update({
-            'issueTokens': True,
-            'failAuthentication': False
-        }) else:
-        # User did not provide the right answer yet (under 3 attempts), present auth challenge
+    if len(session) != 0:
+        print('at least one attempt')
+    elif len(session) == 0:
+        print('no attempts yet')
         response.update({
             'issueTokens': False,
             'failAuthentication': False,
             'challengeName': 'CUSTOM_CHALLENGE'
         })
+
+
+    # if len(session) != 0:
+
+    #     current_session = len(session) - 1
+
+    #     # If user not found, fail auth
+    #     if request.get('userNotFound') is True:
+    #         response.update({
+    #             'issueTokens': False,
+    #             'failAuthentication': True,+
+    #             'msg': "User does not exist"
+    #         })
+    #     # If user does not go through custom auth challenge flow, fail auth
+    #     elif session[current_session].get('challengeName') != 'CUSTOM_CHALLENGE':
+    #         response.update({
+    #             'issueTokens': False,
+    #             'failAuthentication': True,
+    #             'msg': 'User must use custom challenge to sign in'
+    #         })
+    #     # If user attempts 3 times with the wrong OTP, fail auth
+    #     elif len(session) >= 3 and session[2].get('challengeResult') is False:
+    #         response.update({
+    #             'issueTokens': False,
+    #             'failAuthentication': True,
+    #             'msg': 'Incorrect OTP after 3 attempts'
+    #         })
+    #     # Correct custom auth flow and OTP, succeed auth
+    #     elif len(session) >0 and session[current_session].get('challengeName') == 'CUSTOM_CHALLENGE' and session[current_session].get('challengeResult') is True:
+    #         response.update({
+    #             'issueTokens': True,
+    #             'failAuthentication': False
+    #         })
+    # # User did not provide the right answer yet (under 3 attempts), present auth challenge
+    # else:
+    #     response.update({
+    #         'issueTokens': False,
+    #         'failAuthentication': False,
+    #         'challengeName': 'CUSTOM_CHALLENGE'
+    #     })
 
     return event
