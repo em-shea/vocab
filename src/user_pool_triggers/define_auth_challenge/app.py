b71ca15f870c25e7e63de536e6e3af92a1a4535e
Emily Shea <c.emilyshea@gmail.com>
2021-04-06 21:04:50 +0100

    update define auth

--- src/user_pool_triggers/define_auth_challenge/app.py
+++ src/user_pool_triggers/define_auth_challenge/app.py
@@ -6,6 +6,7 @@ def lambda_handler(event, context):
     request = event.get('request')
     session = request.get('session')
 
+
     current_session = len(session) - 1
 
     # If user not found, fail auth
@@ -14,29 +15,25 @@ def lambda_handler(event, context):
             'issueTokens': False,
             'failAuthentication': True,+
             'msg': "User does not exist"
-        })
-    # If user does not go through custom auth challenge flow, fail auth
-    elif session.get('challengeName') != 'CUSTOM_CHALLENGE':
+        }) elif session[current_session].get('challengeName') != 'CUSTOM_CHALLENGE':
+        # If user does not go through custom auth challenge flow, fail auth
         response.update({
             'issueTokens': False,
             'failAuthentication': True,
             'msg': 'User must use custom challenge to sign in'
-        })
-    # If user attempts 3 times with the wrong OTP, fail auth
-    elif len(session) >= 3 and session[2].get('challengeResult') is False:
+        }) elif len(session) >= 3 and session[2].get('challengeResult') is False:
+        # If user attempts 3 times with the wrong OTP, fail auth
         response.update({
             'issueTokens': False,
             'failAuthentication': True,
             'msg': 'Incorrect OTP after 3 attempts'
-        })
-    # Correct custom auth flow and OTP, succeed auth
-    elif len(session) >0 and session[current_session].get('challengeName') == 'CUSTOM_CHALLENGE' and session[current_session].get('challengeResult') is True:
+        }) elif len(session) >0 and session[current_session].get('challengeName') == 'CUSTOM_CHALLENGE' and session[current_session].get('challengeResult') is True:
+        # Correct custom auth flow and OTP, succeed auth
         response.update({
-            'issueTokens': False,
-            'failAuthentication': True
-        })
-    # User did not provide the right answer yet (under 3 attempts), present auth challenge
-    else:
+            'issueTokens': True,
+            'failAuthentication': False
+        }) else:
+        # User did not provide the right answer yet (under 3 attempts), present auth challenge
         response.update({
             'issueTokens': False,
             'failAuthentication': False,
