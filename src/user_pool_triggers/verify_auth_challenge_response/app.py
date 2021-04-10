93a43487528365480f2bc9795ea05b383efda06f
Emily Shea <c.emilyshea@gmail.com>
2021-04-10 11:32:53 +0100

    update verify

--- src/user_pool_triggers/verify_auth_challenge_response/app.py
+++ src/user_pool_triggers/verify_auth_challenge_response/app.py
@@ -7,7 +7,7 @@ import os
 import jwt
 import boto3
 
-cognito_client = boto3.client('cognito-identity', region_name=os.environ['AWS_REGION'])
+cognito_client = boto3.client('cognito-idp', region_name=os.environ['AWS_REGION'])
 
 def lambda_handler(event, context):
 
@@ -22,7 +22,7 @@ def lambda_handler(event, context):
         user_pool_id = event.get('userPoolId')
         user_name = event.get('userName')
 
-        result = cognito_client.admin_update_user_attributes(
+        attribute_update_response = cognito_client.admin_update_user_attributes(
             UserPoolId=user_pool_id,
             Username=user_name,
             UserAttributes=[
