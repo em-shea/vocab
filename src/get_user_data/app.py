17497d921b82a19db5ef941eb1c0a6d42bfedc29
Emily Shea <c.emilyshea@gmail.com>
2021-07-03 13:50:16 +0100

    jwk

--- src/get_user_data/app.py
+++ src/get_user_data/app.py
@@ -1,19 +1,27 @@
 import os
+# import jwt
 import json
 import boto3
+import urllib3
 from boto3.dynamodb.conditions import Key
 
 import sys
 sys.path.insert(0, '/opt')
 
 table = boto3.resource('dynamodb', region_name=os.environ['AWS_REGION']).Table(os.environ['DYNAMODB_TABLE_NAME'])
+f"https://cognito-idp.{os.environ['AWS_REGION']}.amazonaws.com/{os.environ['USER_POOL_ID']}/.well-known/jwks.json"
 
 # For a given user (requires sign-in), return their metadata, subscribed lists, and the past two weeks of quizzes and sentences
 def lambda_handler(event, context):
-    print(event)
 
-    body = json.loads(event["body"])
-    print(body)
+    print(event['headers']['Authorization'])
+    
+    url = f"https://cognito-idp.{os.environ['AWS_REGION']}.amazonaws.com/{os.environ['USER_POOL_ID']}/.well-known/jwks.json"
+
+    http = urllib3.PoolManager()
+    response = http.request('GET', url)
+    print(response.data)
+    
     # response = get_user_data(body['user'])
 
     return
