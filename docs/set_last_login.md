598e6c087cd5887093ed806f7036d000a3f857e2
Emily Shea <c.emilyshea@gmail.com>
2021-10-10 17:23:23 +0100

    last login function

---
+++ docs/set_last_login.md
@@ -0,0 +1,11 @@
+# Set user data API
+
+The set last login function is a POST method at api.haohaotiantian.com/update_login.
+This endpoint updates user metadata to show when the user last signed in.
+
+Example payload:
+````
+{
+    'last_login': '2021-10-10T11:08:04.087480'
+}
+````
---
+++ src/set_last_login/app.py
@@ -0,0 +1,58 @@
+import os
+import json
+import boto3
+import datetime
+from boto3.dynamodb.conditions import Key
+
+table = boto3.resource('dynamodb', region_name=os.environ['AWS_REGION']).Table(os.environ['DYNAMODB_TABLE_NAME'])
+
+# For a given user (requires sign-in), update user metadata
+def lambda_handler(event, context):
+
+    print('event', event)
+    cognito_user_id = event['requestContext']['authorizer']['claims']['sub']
+    print('user id',cognito_user_id)
+
+    error_message = {
+        'statusCode': 502,
+        'headers': {
+            'Access-Control-Allow-Methods': 'POST,OPTIONS',
+            'Access-Control-Allow-Origin': '*',
+        },
+        'body': '{"success" : false}'
+    }
+
+    try:
+        update_last_login(cognito_user_id)
+    except Exception as e:
+        print(f"Error: Failed to update last login - {cognito_user_id}.")
+        print(e)
+        return error_message
+
+    return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Methods': 'POST,OPTIONS',
+                'Access-Control-Allow-Origin': '*',
+            },
+            'body': '{"success" : true}'
+        }
+
+def update_last_login(cognito_user_id):
+
+    response = table.update_item(
+        Key={
+            'PK': "USER#" + cognito_user_id,
+            'SK': "USER#" + cognito_user_id
+        },
+        UpdateExpression="set #login = :u",
+        ExpressionAttributeNames={
+            '#login': 'Last login'
+        },
+        ExpressionAttributeValues={
+            ':u': str(datetime.datetime.now().isoformat())
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+
+    return response
