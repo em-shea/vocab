4524b63b4185deaf8b3ac5cc25241a261eb3c2e6
Emily Shea <c.emilyshea@gmail.com>
2021-11-04 19:02:20 +0100

    get sentences

--- src/get_sentences/app.py
+++ src/get_sentences/app.py
@@ -44,7 +44,6 @@ def pull_user_sentences(congito_id):
 
     response = table.query(
         KeyConditionExpression=Key('PK').eq(user_key) & Key('SK').begins_with('SENTENCE') 
-        # filter by sort key SENTENCES#?
     )
     print('dynamo response ', response['Items'])
     return response['Items']
@@ -55,43 +54,12 @@ def format_user_sentences(sentences_response):
     for item in sentences_response:
         sentence_dict = {}
 
-        sentence_dict['cognito_id'] = item['PK'][5:]
-        sentence_dict['sentence_id'] = item['Email address']
-        sentence_dict['sentence'] = item['sentence']
+        # sentence_dict['cognito_id'] = item['PK'][5:]
+        sentence_dict['sentence_id'] = item['SK'][9:]
+        sentence_dict['sentence'] = item['Sentence']
         sentence_dict['character_set'] = item['Character set']
         sentence_dict['date_created'] = item['Date created']
 
         user_sentences['sentences'].append(sentence_dict)
 
     return user_sentences
-
-# output:
-# {
-#     "sentences":[
-#         {
-#             "sentence_id":"123",
-#             "sentence": "我喜欢学习汉语。",
-#             "date_created": "2021-06-16T23:06:48.467526"
-#             "list_id": "123",
-#             "character_set": "simplified"
-#         },
-#         {
-#             "sentence_id":"234",
-#             "sentence": "我喜欢写句子。",
-#             "date_created": "2021-06-16T23:06:48.467526"
-#             "list_id": "234",
-#             "character_set": "simplified"
-#         }
-#     ]   
-# }
-
-# dynamodb response:
-#         Item = {
-#                 'PK': "USER#" + body['cognito_id'],
-#                 'SK': "SENTENCE#" + body['sentence_id'],
-#                 'Date created': date,
-#                 'List id': body['list_id'],
-#                 'Character set': body['character_set'],
-#                 'GSI1PK': "DATE#" + date,
-#                 'GSI1SK': "USER#" + "SENTENCE#" + body['sentence_id']
-#             }
--- src/tests/unit/test_get_sentences.py
+++ src/tests/unit/test_get_sentences.py
@@ -1,187 +1,209 @@
-# import json
-# import sys
-# sys.path.append('../../')
-# sys.path.append('../../layer/python')
+import json
+import sys
+sys.path.append('../../')
+sys.path.append('../../layer/python')
 
-# import unittest
-# from unittest import mock
+import unittest
+from unittest import mock
 
-# with mock.patch.dict('os.environ', {'AWS_REGION': 'us-east-1', 'TABLE_NAME': 'mock-table'}):
-#     from get_sentences.app import lambda_handler
+with mock.patch.dict('os.environ', {'AWS_REGION': 'us-east-1', 'TABLE_NAME': 'mock-table'}):
+    from get_sentences.app import lambda_handler
 
-# def mocked_pull_user_sentences(cognito_id):
-#     return
+def mocked_pull_user_sentences(cognito_id):
+        example_response = [
+            {
+                "GSI1PK":"DATE#2021-11-04T17:33:56.893897",
+                "Date created":"2021-11-04T17:33:56.893897",
+                "Sentence":"我喜欢韩语。",
+                "SK":"SENTENCE#12345-3763-6260-bf4f-6a03d2d3da0b",
+                "GSI1SK":"USER#SENTENCE#12345-3763-6260-bf4f-6a03d2d3da0b",
+                "PK":"USER#12345-c759-48cd-97ea-3e8876eedb2d",
+                "List id":"12345",
+                "Character set":"simplified"
+            },
+            {
+                "GSI1PK":"DATE#2021-11-04T17:35:16.505909",
+                "Date created":"2021-11-04T17:35:16.505909",
+                "Sentence":"我喜欢法语。",
+                "SK":"SENTENCE#12345-2dd9-6886-bf4f-6a03d2d3da0b",
+                "GSI1SK":"USER#SENTENCE#12345-2dd9-6886-bf4f-6a03d2d3da0b",
+                "PK":"USER#996ca9a4-c759-48cd-97ea-3e8876eedb2d",
+                "List id":"5678",
+                "Character set":"simplified"
+            }
+        ]
+        return example_response
 
-# class GetSentencesTest(unittest.TestCase):
+class GetSentencesTest(unittest.TestCase):
 
-#     @mock.patch('get_sentences.app.pull_user_sentences', side_effect=mocked_pull_user_sentences)
-#     def test_build(self, pull_user_sentences_mock):
+    @mock.patch('get_sentences.app.pull_user_sentences', side_effect=mocked_pull_user_sentences)
+    def test_build(self, pull_user_sentences_mock):
 
-#         response = lambda_handler(self.sub_apig_event(), "")
+        response = lambda_handler(self.sub_apig_event(), "")
 
-#         # self.assertEqual(pull_user_sentences_mock.call_count, 1)
+        # self.assertEqual(pull_user_sentences_mock.call_count, 1)
     
-#     def sub_apig_event(self):
-#         return {
-#             "resource":"/sentence",
-#             "path":"/sentence",
-#             "body":"",
-#             "httpMethod":"GET",
-#             "headers":{
-#                 "Accept":"application/json, text/plain, */*",
-#                 "accept-encoding":"gzip, deflate, br",
-#                 "Accept-Language":"en-US,en;q=0.9,zh-CN;q=0.8,zh-HK;q=0.7,zh-MO;q=0.6,zh;q=0.5",
-#                 "Authorization":"eyJraWQiOiJq1231235fOwKv46JpjurGKzvma17eqCoaw",
-#                 "CloudFront-Forwarded-Proto":"https",
-#                 "CloudFront-Is-Desktop-Viewer":"true",
-#                 "CloudFront-Is-Mobile-Viewer":"false",
-#                 "CloudFront-Is-SmartTV-Viewer":"false",
-#                 "CloudFront-Is-Tablet-Viewer":"false",
-#                 "CloudFront-Viewer-Country":"IE",
-#                 "Host":"api.haohaotiantian.com",
-#                 "origin":"http://localhost:8080",
-#                 "Referer":"http://localhost:8080/",
-#                 "sec-ch-ua":"\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
-#                 "sec-ch-ua-mobile":"?0",
-#                 "sec-fetch-dest":"empty",
-#                 "sec-fetch-mode":"cors",
-#                 "sec-fetch-site":"cross-site",
-#                 "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-#                 "Via":"2.0 f8591238.cloudfront.net (CloudFront)",
-#                 "X-Amz-Cf-Id":"rex4fmbUq5pvK123fj5bGvpw==",
-#                 "X-Amzn-Trace-Id":"Root=1-60e123b7e7b70",
-#                 "X-Forwarded-For":"123",
-#                 "X-Forwarded-Port":"123",
-#                 "X-Forwarded-Proto":"https"
-#             },
-#             "multiValueHeaders":{
-#                 "Accept":[
-#                     "application/json, text/plain, */*"
-#                 ],
-#                 "accept-encoding":[
-#                     "gzip, deflate, br"
-#                 ],
-#                 "Accept-Language":[
-#                     "en-US,en;q=0.9,zh-CN;q=0.8,zh-HK;q=0.7,zh-MO;q=0.6,zh;q=0.5"
-#                 ],
-#                 "Authorization":[
-#                     "eyJraWQiOiJqVmhFdEN4Y123vZ25pdG123GKzvma17eqCoaw"
-#                 ],
-#                 "CloudFront-Forwarded-Proto":[
-#                     "https"
-#                 ],
-#                 "CloudFront-Is-Desktop-Viewer":[
-#                     "true"
-#                 ],
-#                 "CloudFront-Is-Mobile-Viewer":[
-#                     "false"
-#                 ],
-#                 "CloudFront-Is-SmartTV-Viewer":[
-#                     "false"
-#                 ],
-#                 "CloudFront-Is-Tablet-Viewer":[
-#                     "false"
-#                 ],
-#                 "CloudFront-Viewer-Country":[
-#                     "IE"
-#                 ],
-#                 "Host":[
-#                     "api.haohaotiantian.com"
-#                 ],
-#                 "origin":[
-#                     "http://localhost:8080"
-#                 ],
-#                 "Referer":[
-#                     "http://localhost:8080/"
-#                 ],
-#                 "sec-ch-ua":[
-#                     "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\""
-#                 ],
-#                 "sec-ch-ua-mobile":[
-#                     "?0"
-#                 ],
-#                 "sec-fetch-dest":[
-#                     "empty"
-#                 ],
-#                 "sec-fetch-mode":[
-#                     "cors"
-#                 ],
-#                 "sec-fetch-site":[
-#                     "cross-site"
-#                 ],
-#                 "User-Agent":[
-#                     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
-#                 ],
-#                 "Via":[
-#                     "2.0 123.cloudfront.net (CloudFront)"
-#                 ],
-#                 "X-Amz-Cf-Id":[
-#                     "rex4fmbU123BVnGAOV9sfj5bGvpw=="
-#                 ],
-#                 "X-Amzn-Trace-Id":[
-#                     "Root=1-60e6d123b70"
-#                 ],
-#                 "X-Forwarded-For":[
-#                     "123"
-#                 ],
-#                 "X-Forwarded-Port":[
-#                     "443"
-#                 ],
-#                 "X-Forwarded-Proto":[
-#                     "https"
-#                 ]
-#             },
-#             "queryStringParameters":"None",
-#             "multiValueQueryStringParameters":"None",
-#             "pathParameters":"None",
-#             "stageVariables":"None",
-#             "requestContext":{
-#                 "resourceId":"123",
-#                 "authorizer":{
-#                     "claims":{
-#                         "sub":"123123123",
-#                         "aud":"123123",
-#                         "email_verified":"true",
-#                         "event_id":"cc6a7b68-e1bc-417b-9344-123",
-#                         "token_use":"id",
-#                         "auth_time":"1625312024",
-#                         "iss":"https://cognito-idp.us-east-1.amazonaws.com/us-east-1_123",
-#                         "cognito:username":"123123123",
-#                         "exp":"Thu Jul 08 11:38:59 UTC 2021",
-#                         "iat":"Thu Jul 08 10:38:59 UTC 2021",
-#                         "email":"test@email.com"
-#                     }
-#                 },
-#                 "resourcePath":"/user_data",
-#                 "httpMethod":"GET",
-#                 "extendedRequestId":"CJZWoF123FT_Q=",
-#                 "requestTime":"08/Jul/2021:10:38:59 +0000",
-#                 "path":"/user_data",
-#                 "accountId":"123",
-#                 "protocol":"HTTP/1.1",
-#                 "stage":"Prod",
-#                 "domainPrefix":"api",
-#                 "requestTimeEpoch":123,
-#                 "requestId":"11875c1237fec0aab",
-#                 "identity":{
-#                     "cognitoIdentityPoolId":"None",
-#                     "accountId":"None",
-#                     "cognitoIdentityId":"None",
-#                     "caller":"None",
-#                     "sourceIp":"54",
-#                     "principalOrgId":"None",
-#                     "accessKey":"None",
-#                     "cognitoAuthenticationType":"None",
-#                     "cognitoAuthenticationProvider":"None",
-#                     "userArn":"None",
-#                     "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-#                     "user":"None"
-#                 },
-#                 "domainName":"api.haohaotiantian.com",
-#                 "apiId":"123"
-#             },
-#             "isBase64Encoded":False
-#             }
+    def sub_apig_event(self):
+        return {
+            "resource":"/sentence",
+            "path":"/sentence",
+            "body":"",
+            "httpMethod":"GET",
+            "headers":{
+                "Accept":"application/json, text/plain, */*",
+                "accept-encoding":"gzip, deflate, br",
+                "Accept-Language":"en-US,en;q=0.9,zh-CN;q=0.8,zh-HK;q=0.7,zh-MO;q=0.6,zh;q=0.5",
+                "Authorization":"eyJraWQiOiJq1231235fOwKv46JpjurGKzvma17eqCoaw",
+                "CloudFront-Forwarded-Proto":"https",
+                "CloudFront-Is-Desktop-Viewer":"true",
+                "CloudFront-Is-Mobile-Viewer":"false",
+                "CloudFront-Is-SmartTV-Viewer":"false",
+                "CloudFront-Is-Tablet-Viewer":"false",
+                "CloudFront-Viewer-Country":"IE",
+                "Host":"api.haohaotiantian.com",
+                "origin":"http://localhost:8080",
+                "Referer":"http://localhost:8080/",
+                "sec-ch-ua":"\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
+                "sec-ch-ua-mobile":"?0",
+                "sec-fetch-dest":"empty",
+                "sec-fetch-mode":"cors",
+                "sec-fetch-site":"cross-site",
+                "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+                "Via":"2.0 f8591238.cloudfront.net (CloudFront)",
+                "X-Amz-Cf-Id":"rex4fmbUq5pvK123fj5bGvpw==",
+                "X-Amzn-Trace-Id":"Root=1-60e123b7e7b70",
+                "X-Forwarded-For":"123",
+                "X-Forwarded-Port":"123",
+                "X-Forwarded-Proto":"https"
+            },
+            "multiValueHeaders":{
+                "Accept":[
+                    "application/json, text/plain, */*"
+                ],
+                "accept-encoding":[
+                    "gzip, deflate, br"
+                ],
+                "Accept-Language":[
+                    "en-US,en;q=0.9,zh-CN;q=0.8,zh-HK;q=0.7,zh-MO;q=0.6,zh;q=0.5"
+                ],
+                "Authorization":[
+                    "eyJraWQiOiJqVmhFdEN4Y123vZ25pdG123GKzvma17eqCoaw"
+                ],
+                "CloudFront-Forwarded-Proto":[
+                    "https"
+                ],
+                "CloudFront-Is-Desktop-Viewer":[
+                    "true"
+                ],
+                "CloudFront-Is-Mobile-Viewer":[
+                    "false"
+                ],
+                "CloudFront-Is-SmartTV-Viewer":[
+                    "false"
+                ],
+                "CloudFront-Is-Tablet-Viewer":[
+                    "false"
+                ],
+                "CloudFront-Viewer-Country":[
+                    "IE"
+                ],
+                "Host":[
+                    "api.haohaotiantian.com"
+                ],
+                "origin":[
+                    "http://localhost:8080"
+                ],
+                "Referer":[
+                    "http://localhost:8080/"
+                ],
+                "sec-ch-ua":[
+                    "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\""
+                ],
+                "sec-ch-ua-mobile":[
+                    "?0"
+                ],
+                "sec-fetch-dest":[
+                    "empty"
+                ],
+                "sec-fetch-mode":[
+                    "cors"
+                ],
+                "sec-fetch-site":[
+                    "cross-site"
+                ],
+                "User-Agent":[
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+                ],
+                "Via":[
+                    "2.0 123.cloudfront.net (CloudFront)"
+                ],
+                "X-Amz-Cf-Id":[
+                    "rex4fmbU123BVnGAOV9sfj5bGvpw=="
+                ],
+                "X-Amzn-Trace-Id":[
+                    "Root=1-60e6d123b70"
+                ],
+                "X-Forwarded-For":[
+                    "123"
+                ],
+                "X-Forwarded-Port":[
+                    "443"
+                ],
+                "X-Forwarded-Proto":[
+                    "https"
+                ]
+            },
+            "queryStringParameters":"None",
+            "multiValueQueryStringParameters":"None",
+            "pathParameters":"None",
+            "stageVariables":"None",
+            "requestContext":{
+                "resourceId":"123",
+                "authorizer":{
+                    "claims":{
+                        "sub":"123123123",
+                        "aud":"123123",
+                        "email_verified":"true",
+                        "event_id":"cc6a7b68-e1bc-417b-9344-123",
+                        "token_use":"id",
+                        "auth_time":"1625312024",
+                        "iss":"https://cognito-idp.us-east-1.amazonaws.com/us-east-1_123",
+                        "cognito:username":"123123123",
+                        "exp":"Thu Jul 08 11:38:59 UTC 2021",
+                        "iat":"Thu Jul 08 10:38:59 UTC 2021",
+                        "email":"test@email.com"
+                    }
+                },
+                "resourcePath":"/user_data",
+                "httpMethod":"GET",
+                "extendedRequestId":"CJZWoF123FT_Q=",
+                "requestTime":"08/Jul/2021:10:38:59 +0000",
+                "path":"/user_data",
+                "accountId":"123",
+                "protocol":"HTTP/1.1",
+                "stage":"Prod",
+                "domainPrefix":"api",
+                "requestTimeEpoch":123,
+                "requestId":"11875c1237fec0aab",
+                "identity":{
+                    "cognitoIdentityPoolId":"None",
+                    "accountId":"None",
+                    "cognitoIdentityId":"None",
+                    "caller":"None",
+                    "sourceIp":"54",
+                    "principalOrgId":"None",
+                    "accessKey":"None",
+                    "cognitoAuthenticationType":"None",
+                    "cognitoAuthenticationProvider":"None",
+                    "userArn":"None",
+                    "userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+                    "user":"None"
+                },
+                "domainName":"api.haohaotiantian.com",
+                "apiId":"123"
+            },
+            "isBase64Encoded":False
+            }
 
-#     if __name__ == '__main__':
-#         unittest.main()
+    if __name__ == '__main__':
+        unittest.main()
