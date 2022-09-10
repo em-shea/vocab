56e061eb4ab7cbf1abb57c248989423ca4260e14
Emily Shea <c.emilyshea@gmail.com>
2022-09-10 10:48:35 +0100

    event pattern account

--- src/post_tweet/app.py
+++ src/post_tweet/app.py
@@ -1,5 +1,6 @@
 import os
 import boto3
+from boto3.dynamodb.conditions import Key
 
 idempotency_table = boto3.resource('dynamodb', region_name=os.environ['AWS_REGION']).Table(os.environ['IDEMPOTENCY_TABLE'])
 
@@ -7,10 +8,17 @@ def lambda_handler(event, context):
 
     print('event: ', event)
 
-    check_idempotency_key()
+    check_idempotency_key(event['Detail']['idempotency-key'])
+
 
     return
 
-def check_idempotency_key():
+def check_idempotency_key(idempotency_key):
 
-    # query
+    user_key = "USER#" + cognito_id
+
+    response = idempotency_table.query(
+        KeyConditionExpression=Key('IdempotencyKey').eq(user_key) & Key('Consumer').eq('PostTweet')
+    )
+    print('dynamo response ', response['Items'])
+    return response['Items']
