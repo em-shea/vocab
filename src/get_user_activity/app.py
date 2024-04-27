3466d04f816bd0de9a0d7cdb3c4b12e274067799
em-shea <c.emilyshea@gmail.com>
2024-04-27 16:23:39 +0100

    get user acitivity

--- src/get_user_activity/app.py
+++ src/get_user_activity/app.py
@@ -1,15 +1,23 @@
 import json
+import user_service
+import review_word_service
 from dataclasses import asdict
-
-import user_activity_service
 
 # For a given user (requires sign-in), return their metadata, subscribed lists, and the past two weeks of quizzes and sentences
 def lambda_handler(event, context):
     # print('event', event)
     cognito_id = event['requestContext']['authorizer']['claims']['sub']
     print('user id',cognito_id)
+    date_range = 10
     
-    user = user_activity_service.get_user_activity(cognito_id)
+    user_data = user_service.get_single_user_with_activity(cognito_id, date_range)
+    print('user activity: ', user_data)
+    
+    # Add recent words for subscribed lists
+
+    # for list in user_data['subscriptions']:
+    #    review_word_service.get_review_words(list['list_id'], date_range)
+        # append
 
     # print('user: ', user)
 
@@ -22,12 +30,12 @@ def lambda_handler(event, context):
 
     # user.subscriptions = subscribed_lists
 
-    print('user: ', user)
+    print('user: ', user_data)
     return {
         'statusCode': 200,
         'headers': {
             'Access-Control-Allow-Methods': 'GET,OPTIONS',
             'Access-Control-Allow-Origin': '*',
         },
-        'body': json.dumps(asdict(user))
+        'body': json.dumps(asdict(user_data))
     }
